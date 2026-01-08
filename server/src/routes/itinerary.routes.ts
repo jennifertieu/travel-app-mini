2fd82c7198a9b630669d653baa3cc12ca95a3edd
Alex Appleget <alexappleget2014@gmail.com>
2026-01-07 21:26:01 -0600

    added ai function to itinerary route (forgot)

--- server/src/routes/itinerary.routes.ts
+++ server/src/routes/itinerary.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { requireAuth } from "../middleware/requireAuth";
+import { createItinerary } from "../controllers/itinerary.controller";
 
 const router = express.Router();
 
-router.post("/:id", requireAuth);
+router.post("/:id", requireAuth, createItinerary);
 
 export default router;
