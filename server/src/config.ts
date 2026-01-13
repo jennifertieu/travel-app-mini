705bf5162fbec1938006fee2fe33b08a18e27b20
Jennifer Tieu <jennifer.tieuu@gmail.com>
2026-01-12 21:13:20 -0600

    fix: export google maps platform api key

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,8 +7,7 @@ dotenv.config();
 export const PORT =
   process.env.PORT === "5000" ? "5001" : process.env.PORT || "5001";
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY!;
-export const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY!;
-export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY!;
+export const GOOGLE_MAPS_PLATFORM_API_KEY = process.env.GOOGLE_MAPS_PLATFORM_API_KEY!;
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
