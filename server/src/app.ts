baf3fb76b7351a976cb8bd6a8bdc2e6204f8f2c6
Jennifer Tieu <jennifer.tieuu@gmail.com>
2026-02-02 19:07:34 -0600

    fix: remove unused module import

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,6 @@ import enrichmentRoutes from "./routes/enrichment.routes.js";
 import itineraryRoutes from "./routes/itinerary.routes.js";
 import duringTripRoutes from "./routes/duringTrip.routes.js";
 import suggestionsRoutes from "./routes/suggestions.routes.js";
-import collaborationRoutes from "./routes/collaboration.routes.js";
 import express from "express";
 import { PORT } from "./config.js";
 import cors from "cors";
@@ -26,7 +25,6 @@ app.use("/itinerary", itineraryRoutes);
 app.use("/enrich", enrichmentRoutes);
 app.use("/during-trip", duringTripRoutes);
 app.use("/suggestions", suggestionsRoutes);
-app.use("/collaboration", collaborationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
